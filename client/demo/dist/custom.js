8d1ff967bf873c1756ab976308d165a5b0fd9365
Cyrus Chan <cyrus.chan@star.hku.hk>
2025-02-15 00:23:12 +0000

    Hide welcome dialog

--- client/demo/dist/custom.js
+++ client/demo/dist/custom.js
@@ -1,5 +1,13 @@
 (function(){
-    document.querySelectorAll('#open-starting-notice-dialog-checkbox').forEach( (item) => {
-        item.checked = false;
-    });
+    let timer = setInterval(() => {
+        let welcomeMsgCheckbox = document.querySelectorAll('#open-starting-notice-dialog-checkbox');
+        
+        welcomeMsgCheckbox.forEach( (item) => {
+            item.checked = false;
+        });
+
+        if (welcomeMsgCheckbox) {
+            clearInterval(timer);
+        }
+    }, 1000);    
 })();
